01dd4c09a643c492d2121a2779cdc187ceb18219
Adam <argrgic@gmail.com>
2019-04-25 15:37:33 -0500

    Created package.json files

--- shopping-cart/app.js
+++ shopping-cart/app.js
@@ -1,16 +1,13 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-const app = express();
-// eslint-disable-next-line no-undef
-mongoose.connect('mongodb://localhost:27017/Grgic', { useNewUrlParser: true }, (err));
-
+var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -26,12 +23,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use((req, res, next) => {
+app.use(function(req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use((err, req, res) => {
+app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
